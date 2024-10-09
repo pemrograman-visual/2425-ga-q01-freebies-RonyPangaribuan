c0c37838e020979902395f140c0f8ffbc11d3636
rony <ronypangaribuan2505@gmail.com>
2024-10-09 16:07:44 +0700

    PAIR17

--- X01.java
+++ X01.java
@@ -1,2 +1,38 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24040 - Rony Reynaldy Pangaribuan
+// 12S24023 - Jaya Bestina Simbolon
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double bukuMurah, hargaBuku, total;
+
+        total = 0;
+        bukuMurah = 9999;
+        do {
+            hargaBuku = Double.parseDouble(input.nextLine());
+            if (hargaBuku == 0) {
+            } else {
+                if (hargaBuku < bukuMurah) {
+                    bukuMurah = hargaBuku;
+                } else {
+                    hargaBuku = hargaBuku;
+                }
+                total = total + hargaBuku;
+            }
+        } while (hargaBuku != 0);
+        if (total > 0) {
+            total = total - bukuMurah;
+        } else {
+            total = total;
+        }
+        System.out.println(toFixed(total,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
