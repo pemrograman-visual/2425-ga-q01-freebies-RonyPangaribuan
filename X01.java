5954883738dc20cb8dae5634ccd4a3616ea8d609
rony <ronypangaribuan2505@gmail.com>
2024-10-09 16:44:20 +0700

    Update X01.java

--- X01.java
+++ X01.java
@@ -8,24 +8,25 @@ public class X01 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double bukuMurah, hargaBuku, total;
+        double bukumurah, hargaBuku, total;
 
         total = 0;
-        bukuMurah = 9999;
-        do {
-            hargaBuku = Double.parseDouble(input.nextLine());
+        bukumurah = 9999;
+        hargaBuku = Double.parseDouble(input.nextLine());
+        while (hargaBuku != 0) {
             if (hargaBuku == 0) {
             } else {
-                if (hargaBuku < bukuMurah) {
-                    bukuMurah = hargaBuku;
+                if (hargaBuku < bukumurah) {
+                    hargaBuku = bukumurah;
                 } else {
                     hargaBuku = hargaBuku;
                 }
                 total = total + hargaBuku;
             }
-        } while (hargaBuku != 0);
-        if (total > 0) {
-            total = total - bukuMurah;
+            hargaBuku = Double.parseDouble(input.nextLine());
+        }
+        if (total >= 100) {
+            total = total - bukumurah;
         } else {
             total = total;
         }
